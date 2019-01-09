10aa454f59872acc4664f4ef02de581d1f65d165
Benedek László <lacbenedek@gmail.com>
2019-01-09 21:09:58 +0100

    started converting
    obj_loader.py/load now returns the lists
    in modern OpenGL compatible format

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from pygame.locals import *
 import obj_loader as obj
 
 from pyrr import Quaternion, Matrix44, Vector3
-#import numpy as np
+import numpy as np
 
 
 print("\nOpenGL Python engine")
@@ -18,27 +18,50 @@ dev = True          #Additional information in the console about things happenin
 display = (640,480) #Drawing image size
 
 objects = []        #Contains all objects that had been loaded and can be drawn
+buffers = []
+ibos = []
 
-
-
-def Modify(object_id,pos,rot,scale):
-                
-    translation = Vector3()
-    translation += pos
-                
-    scale = Vector3(scale)
-
-    matrix = Matrix44.identity()
-    matrix = matrix * Matrix44.from_translation(translation)
-    matrix = matrix * Matrix44.from_scale(scale)
+def CreateBufferForObjects():
 
     i = 0
-    for point in objects[object_id].vertices:
-        v = Vector3(point)
-        v = matrix * v
-        point = [v.x,v.y,v.z]
-        objects[object_id].vertices[i] = point
-        i+=1
+    for _object in objects:
+        x = 0
+        glGenBuffers(1,x)
+        buffers.append(x)
+        print("B: " + str(buffers))
+        
+        glGenBuffers(1,x)
+        ibos.append(x)
+        print("I: " + str(ibos))
+
+        
+        glBindBuffer(GL_ARRAY_BUFFER, buffers[i])
+        glBufferData(GL_ARRAY_BUFFER, len(_object.vertices) * np.array(_object.vertices, dtype="float32").itemsize, np.array(_object.vertices, dtype="float32"), GL_STATIC_DRAW)
+        glVertexAttribPointer(0,3,GL_FLOAT,GL_FALSE, 24 * 3, 0)
+        glEnableVertexAttribArray(0)
+
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibos[i])
+        glBufferData(GL_ARRAY_BUFFER, len(_object.order) * 24, _object.order, GL_STATIC_DRAW)
+        
+
+##def Modify(object_id,pos,rot,scale):
+##                
+##    translation = Vector3()
+##    translation += pos
+##                
+##    scale = Vector3(scale)
+##
+##    matrix = Matrix44.identity()
+##    matrix = matrix * Matrix44.from_translation(translation)
+##    matrix = matrix * Matrix44.from_scale(scale)
+##
+##    i = 0
+##    for point in objects[object_id].vertices:
+##        v = Vector3(point)
+##        v = matrix * v
+##        point = [v.x,v.y,v.z]
+##        objects[object_id].vertices[i] = point
+##        i+=1
 
         
 def Load_map(path):
@@ -53,23 +76,15 @@ def Load_map(path):
                 objects.append(obj.load(parts[0]))
 
                 rot = [0,0,0]
-                Modify(len(objects)-1,[float(parts[1].split(",")[0]),float(parts[1].split(",")[1]), float(parts[1].split(",")[2])],rot,[float(parts[3].split(",")[0]),float(parts[3].split(",")[1]), float(parts[3].split(",")[2])])
+                #Modify(len(objects)-1,[float(parts[1].split(",")[0]),float(parts[1].split(",")[1]), float(parts[1].split(",")[2])],rot,[float(parts[3].split(",")[0]),float(parts[3].split(",")[1]), float(parts[3].split(",")[2])])
 
-                if dev:
-                    print("INFO: Loaded object with name '" + objects[len(objects)-1].name + "' from path '" + parts[0] + "'. Its position in the world is '" + str([float(parts[1].split(",")[0]),float(parts[1].split(",")[1]), float(parts[1].split(",")[2])]) + "' and its rotaition is '" + str(rot) + "'.")
+                #if dev:
+                #    print("INFO: Loaded object with name '" + objects[len(objects)-1].name + "' from path '" + parts[0] + "'. Its position in the world is '" + str([float(parts[1].split(",")[0]),float(parts[1].split(",")[1]), float(parts[1].split(",")[2])]) + "' and its rotaition is '" + str(rot) + "'.")
 
-def Render(otd):
-    glBegin(GL_TRIANGLES)
-    for face in otd.faces:
-        e = 0
-        for vertex in face.vertices:
-            glNormal3f(otd.normals[face.normals[e]][0],otd.normals[face.normals[e]][1],otd.normals[face.normals[e]][2])
-            glVertex3fv(otd.vertices[vertex])
-            
-            e+=1
-            if e >= 3:
-                e = 0
-    glEnd()
+def Render(index):
+    glBindBuffer(buffers[index])
+    glBindBuffer(ibos[index])
+    glDrawElements(GL_TRIANGLES, len(objects[index].order), GL_UNSIGNED_INT, None)
     
     
 def Main():
@@ -94,13 +109,10 @@ def Main():
 
     #Custom setup, loading objects
     Load_map("./maps/test.map")
+    CreateBufferForObjects()
 
     #Rendering loop, done once a frame
     while 1:
-##        for event in pygame.event.get():
-##            if event.type == pygame.QUIT:
-##                pygame.quit()
-##                quit()
 
         glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
 
--- obj_loader.py
+++ obj_loader.py
@@ -1,31 +1,37 @@
-#import numpy as np
-#from pyrr import Vector3
+#MODERN OPENGL SUPPORT
 
-class Face:
-    def __init__(self, vert, textc, norm):
-        self.vertices = vert
-        self.texture_cordinates = textc
-        self.normals = norm
+
+#class Face:
+    #def __init__(self, vert, textc, norm):
+        #self.vertices = vert
+        #self.texture_cordinates = textc
+        #self.normals = norm
+
+    #vertices = []
+    #texture_cordinates = []
+    #normals = []
         
 
 class Object:
-    def __init__(self,vertices,faces,texture_cordinates,normals,name,path):
+    def __init__(self,vertices,order,order_textures,order_normals,texture_cordinates,normals,name,path):
+
+        self.order = order
+        self.order_textures = order_textures
+        self.order_normals = order_normals
+
         self.vertices = vertices
-        self.faces = faces
         self.texture_cordinates = texture_cordinates
         self.normals = normals
         
         self.name = name
         self.path = path
 
-        #self.pos = pos
-        #self.rot = rot
-        #self.scale = scale
-
         
 def load(path):
     vertices = []
-    faces = []
+    order = []
+    order_textures = []
+    order_normals = []
     texture_cordinates = []
     normals = []
     name = "Unknown model"
@@ -35,8 +41,12 @@ def load(path):
             #Vertices
             if "v " in line:
                 parts = line.split(" ")
-                vertex = [float(parts[1]),float(parts[2]),float(parts[3])]
-                vertices.append(vertex)
+                
+                #vertex = [float(parts[1]),float(parts[2]),float(parts[3])]
+                
+                vertices.append(float(parts[1]))
+                vertices.append(float(parts[2]))
+                vertices.append(float(parts[3]))
                 
 
             #Faces
@@ -44,30 +54,53 @@ def load(path):
                 parts = line.split(" ")
                 del(parts[0])
 
-                _vertices = [int(parts[0].split("/")[0])-1, int(parts[1].split("/")[0])-1, int(parts[2].split("/")[0])-1]
-                _texture_cordinates = [int(parts[0].split("/")[1])-1, int(parts[1].split("/")[1])-1, int(parts[2].split("/")[1])-1]
-                _normals = [int(parts[0].split("/")[2])-1, int(parts[1].split("/")[2])-1, int(parts[2].split("/")[2])-1]
-                faces.append(Face(_vertices,_texture_cordinates,_normals))
+                #_vertices = [int(parts[0].split("/")[0])-1, int(parts[1].split("/")[0])-1, int(parts[2].split("/")[0])-1]
+                #_texture_cordinates = [int(parts[0].split("/")[1])-1, int(parts[1].split("/")[1])-1, int(parts[2].split("/")[1])-1]
+                #_normals = [int(parts[0].split("/")[2])-1, int(parts[1].split("/")[2])-1, int(parts[2].split("/")[2])-1]
+                #faces.append(Face(_vertices,_texture_cordinates,_normals))
+
+                order.append(int(parts[0].split("/")[0])-1)
+                order.append(int(parts[1].split("/")[0])-1)
+                order.append(int(parts[2].split("/")[0])-1)
+
+                order_textures.append(int(parts[0].split("/")[1])-1)
+                order_textures.append(int(parts[1].split("/")[1])-1)
+                order_textures.append(int(parts[2].split("/")[1])-1)
+                
+                order_normals.append(int(parts[0].split("/")[2])-1)
+                order_normals.append(int(parts[1].split("/")[2])-1)
+                order_normals.append(int(parts[2].split("/")[2])-1)
+
                 
 
             #Texture cordinates
             if "vt " in line:
                 parts = line.split(" ")
-                cordinate = [float(parts[1]),float(parts[2])]
-                texture_cordinates.append(cordinate)
+                
+                #cordinate = [float(parts[1]),float(parts[2])]
+                #texture_cordinates.append(cordinate)
+                
+                texture_cordinates.append(float(parts[1]))
+                texture_cordinates.append(float(parts[2]))
                 
 
             #Normals
             if "vn " in line:
                 parts = line.split(" ")
-                normal = [float(parts[1]),float(parts[2]),float(parts[3])]
-                normals.append(normal)
+                
+                #normal = [float(parts[1]),float(parts[2]),float(parts[3])]
+                #normals.append(normal)
+
+                normals.append(float(parts[1]))
+                normals.append(float(parts[2]))
+                normals.append(float(parts[3]))
 
             #Name
             if "o " in line:
                 name = line[2:len(line)-1]
 
-    return(Object(vertices, faces, texture_cordinates, normals,name,path))
+    return(Object(vertices,order,order_textures,order_normals,texture_cordinates,normals,name,path))
 
 #Test:          
-#load("D:\\xd.obj")
+obj = load("D:\\xd.obj")
+print(obj.order_textures)
